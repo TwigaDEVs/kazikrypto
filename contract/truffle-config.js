d977fbbd2ba94add4e9559154784618597bab08f
Onchez <56371447+onchezz@users.noreply.github.com>
2023-07-23 17:29:39 +0300

    Update truffle-config.js

--- contract/truffle-config.js
+++ contract/truffle-config.js
@@ -43,12 +43,12 @@ module.exports = {
     // options below to some value.
     //
     ganache: {
+    network_id: "*",
     host: "localhost",
     port: 8545 // Standard Ethereum port
    },
     development: {
-     // host: "127.0.0.1",     // Localhost (default: none)
-     host: "0.0.0.0",
+     host: "127.0.0.1",     // Localhost (default: none
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
      gas: 5000000
