4b39e88baef8a4f95ebd6a0990e866574481d8ad
onchezz <brianonchezz@gmail.com>
2023-07-24 20:32:52 +0300

    check script

--- contract/check-coverage.sh
+++ contract/check-coverage.sh
@@ -1,28 +1,63 @@
 #!/bin/bash
-# cd contract
+
 # Compile contracts
 truffle compile
 
-# Extract function signatures from artifacts
+# Extract all functions 
 ARTIFACTS=$(find build/contracts -name "*.json")
-> compiled.txt
+> all_functions.txt
 
 for FILE in $ARTIFACTS; do
-  FUNCTIONS=$(cat $FILE | jq -r '.abi | map(select(.type == "function")) | .[].signature' )
-  echo $FUNCTIONS >> compiled.txt
+  FUNCTIONS=$(cat $FILE | jq -r '.abi | map(select(.type == "function")) | .[].signature')
+  echo $FUNCTIONS >> all_functions.txt
 done
 
-# Get called functions from tests
-grep -E 'functionName\(.*\)' test/*.js | sed 's/^.*(\(.*\)).*$/\1/' > tested.txt
+# Get tested functions
+grep -E 'functionName\(.*\)' test/*.js | sed 's/^.*(\(.*\)).*$/\1/' > tested_functions.txt
 
-# Compare
-diff compiled.txt tested.txt
+# Compare 
+diff all_functions.txt tested_functions.txt
 
-# Check for diff
+# Output results
 if [ $? -eq 0 ]; then
+
+  echo "Tested Functions:"
+  cat tested_functions.txt
+
   echo "All functions covered!"
   exit 0
-else 
+
+else
+
   echo "Missing function coverage!"
   exit 1
+
 fi
+# #!/bin/bash
+# # cd contract
+# # Compile contracts
+# truffle compile
+
+# # Extract function signatures from artifacts
+# ARTIFACTS=$(find build/contracts -name "*.json")
+# > compiled.txt
+
+# for FILE in $ARTIFACTS; do
+#   FUNCTIONS=$(cat $FILE | jq -r '.abi | map(select(.type == "function")) | .[].signature' )
+#   echo $FUNCTIONS >> compiled.txt
+# done
+
+# # Get called functions from tests
+# grep -E 'functionName\(.*\)' test/*.js | sed 's/^.*(\(.*\)).*$/\1/' > tested.txt
+
+# # Compare
+# diff compiled.txt tested.txt
+
+# # Check for diff
+# if [ $? -eq 0 ]; then
+#   echo "All functions covered!"
+#   exit 0
+# else 
+#   echo "Missing function coverage!"
+#   exit 1
+# fi
