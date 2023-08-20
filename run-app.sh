8803aabd1ce19f78897af749bc308efc64fb486a
Onchez <56371447+onchezz@users.noreply.github.com>
2023-08-20 20:17:47 +0300

    Create run-app.sh

---
+++ run-app.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Repository URL
+REPO="https://github.com/TwigaDEVs/kazikrypto.git" 
+
+# Branch name
+BRANCH="master"
+
+# Install command
+INSTALL="npm install" 
+
+# dev command 
+DEV="npm run dev"
+
+# Clone repo
+git clone  $REPO
+
+# Change to repo directory 
+cd web-frontend
+
+# Install dependencies
+$INSTALL
+
+# run web
+$DEV
